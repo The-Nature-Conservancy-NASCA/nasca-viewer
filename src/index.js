128dd8eac021b3c066f870850502a34485535b3c
Pablo Nieto <nietop@google.com>
2019-10-21 09:49:47 -0500

    Landing dinámico 2

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class Landing {
       estrategia.addEventListener('click', event => {
         const selectedId = event.currentTarget.dataset.estrategia;
         document.querySelector('.estrategias').classList.add('collapsed');
-        showProyectos(selectedId);
+        this.showProyectos(selectedId);
       });
     });
   }
@@ -25,12 +25,7 @@ class Landing {
   }
   
   loadLandingData() {
-   /* window.setTimeout(() => {
-     document.querySelector('.content').classList.remove('hidden');
-     document.querySelector('.loader').classList.add('hidden');
-    }, 2000);*/
-    
-    require(['esri/request'], esriRequest => {
+     require(['esri/request'], esriRequest => {
       const queryOptions = {
         query: {
           f: 'json',
@@ -40,31 +35,68 @@ class Landing {
         },
         responseType: 'json'
       };
+
+      const estrategiasRequest = esriRequest(urlEstrategias, queryOptions);
+      const proyectosRequest = esriRequest(urlProyectos, queryOptions);
       
-      esriRequest(urlEstrategias, queryOptions).then(response => {
-        let estrategias = [];
-        if (response.data && response.data.features) {
-          const { features } = response.data;
-          estrategias = features.map(feature => feature.attributes);
-        }
-        let estrategiasHML = this.createHTMLEstrategias(estrategias);
-        document.querySelector('.estrategias').innerHTML = estrategiasHML;
-        this.registerHandlers();
-        document.querySelector('.content').classList.remove('hidden');
-        document.querySelector('.loader').classList.add('hidden');
-      });
+      Promise.all([estrategiasRequest, proyectosRequest]).then(this.processResponse.bind(this));
     });
   }
   
-  createHTMLEstrategias(estrategias) {
+  processResponse(responses) {
+    const estrategiasResponse = responses[0];
+    const proyectosResponse = responses[1];
+
+    this.buildEstrategiasHTML(estrategiasResponse);
+    this.buildProyectosHTML(proyectosResponse);
+
+    this.registerHandlers();
+    document.querySelector('.content').classList.remove('hidden');
+    document.querySelector('.loader').classList.add('hidden');
+    
+  }
+
+  buildEstrategiasHTML(estrategiasResponse) {
+    let estrategias = [];
+    if (estrategiasResponse.data && estrategiasResponse.data.features) {
+      const { features } = estrategiasResponse.data;
+      estrategias = features.map(feature => feature.attributes);
+    }
+
+    let estrategiasHTML = this.createHTML(estrategias, 'estrategias');
+    document.querySelector('.estrategias').innerHTML = estrategiasHTML;
+  }
+
+  buildProyectosHTML(proyectosResponse) {
+    let proyectos = {};
+    if (proyectosResponse.data && proyectosResponse.data.features) {
+      const { features } = proyectosResponse.data;
+      Array.from(features, f => f.attributes).forEach(proyecto => {
+        if (!proyectos[proyecto.ID_estrategia]) {
+          proyectos[proyecto.ID_estrategia] = new Array();
+        }
+        proyectos[proyecto.ID_estrategia].push(proyecto);
+      });
+    }
+
+    for (const estrategiaID in proyectos) {
+      let divProyecto = document.createElement('div');
+      divProyecto.setAttribute('id', `estrategia_${estrategiaID}`);
+      divProyecto.classList.add('proyectos', 'hidden');
+      divProyecto.innerHTML = this.createHTML(proyectos[estrategiaID], 'proyectos');
+      document.querySelector('.content').appendChild(divProyecto);
+    }
+  }
+
+  createHTML(sections, nivel) {
     let html = '';
     
-    estrategias.forEach(estrategia => {
+    sections.forEach(section => {
       html += this.hidrateCard({
-        nivel: 'estrategias',
-        ...estrategia
+        nivel,
+        ...section
       })
-    })
+    });
     
     return html;
   }
@@ -72,9 +104,8 @@ class Landing {
   hidrateCard(options) {
     return `
       <section class="${options.nivel}__card" 
-      ${options.ID_proyecto ? 
-        'id=' + options.nombre_estrategia :
-        'data-estrategia="' + options.nombre_estrategia + '"'}>
+        ${options.ID_proyecto ? '':
+        'data-estrategia="estrategia_' + options.ID_estrategia + '"'}>
       <div class="${options.nivel}__text">
         <h2 class="${options.nivel}__title">${options.nombre_estrategia}</h2>
         <p class="${options.nivel}__description">${options.descripcion}</p>
