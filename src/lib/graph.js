2a0b00d2ee292a0009549f528531173443e017e5
Marcelo Villa <marcelo.villa.p@gmail.com>
2019-11-04 10:31:20 -0500

    teest treemap

---
+++ src/lib/graph.js
@@ -0,0 +1,95 @@
+class TreeMap {
+
+  constructor (el) {
+    const margin = {top: 10, right: 10, bottom: 10, left: 10};
+    this.width = 300 - margin.left - margin.right;
+    this.height = 300 - margin.top - margin.bottom;
+
+    this.color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    this.svg = d3.select(el)
+                  .append("svg")
+                    .attr("width", this.width + margin.left + margin.right)
+                    .attr("height", this.height + margin.top + margin.bottom)
+                  .append("g")
+                    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+    this.constants = {
+      NAME: "coberturas",
+      PARENT_LABEL: "cobertura_actual",
+      CHILD_LABEL: "sub_cobertura_actual",
+      VALUE_FIELD: "porcentaje_area"
+    };
+  }
+
+  _stratify (features, name, parentLabel, childLabel, valueField) {
+    const data = {"name": name, "children": []};
+    for (let feat of features) {
+      const parentExists = !!data.children.filter(child => child.name === feat.attributes[parentLabel]).length;
+      if (parentExists) {
+        const parent = data.children.filter(child => child.name === feat.attributes[parentLabel])[0]
+        const childrenExists = !!parent.children.filter(child => child.name === feat.attributes[childLabel]).length;
+        if (childrenExists) {
+          const childEl = parent.children.filter(child => child.name === feat.attributes[childLabel])[0];
+          childEl.value += feat.attributes[valueField];
+        } else {
+          const obj = {
+            "name": feat.attributes[childLabel],
+            "value": feat.attributes[valueField]
+          };
+          parent.children.push(obj);
+        }
+      } else {
+        const obj = {
+          "name": feat.attributes[parentLabel],
+          "children": [{
+            "name": feat.attributes[childLabel],
+            "value": feat.attributes[valueField]
+          }]
+        };
+        data.children.push(obj);
+      }
+    }
+    return data;
+  }
+
+  _renderTreeMap (data) {
+    const root = d3.hierarchy(data).sum(d => d.value);
+
+    // create treemap layout
+    d3.treemap()
+      .size([this.width, this.height])
+      .padding(3)
+      // .round(true)
+      (root);
+
+    // add rectangles
+    this.svg.selectAll("rect")
+          .data(root.leaves())
+          .enter()
+          .append("rect")
+            .attr('x', d => d.x0)
+            .attr('y', d => d.y0)
+            .attr('width', d => d.x1 - d.x0)
+            .attr('height', d => d.y1 - d.y0)
+            // .attr("fill-opacity", 0.6)
+            .style("fill", d => this.color(d.parent.data.name));
+      
+    // this.svg.selectAll("text")
+    //       .data(root.leaves())
+    //       .enter()
+    //       .append("text")
+    //         .attr("clip-path", d => d.clipUid)
+    //         .attr("x", d => d.x0 + 3)
+    //         .attr("y", d => d.y0 + 14)
+    //         .text(d => d.data.name)
+    //         .attr("font-size", "12px")
+    //         .attr("fill", "white");
+  }
+
+  renderGraphic (features) {
+    const data = this._stratify(features, this.constants.NAME, this.constants.PARENT_LABEL, this.constants.CHILD_LABEL, this.constants.VALUE_FIELD);
+    this._renderTreeMap(data);
+  }
+
+}
--- src/lib/map.js
+++ src/lib/map.js
@@ -2,15 +2,15 @@ class TNCMap {
 
   constructor(container) {
     
-    require(['esri/WebMap', 'esri/views/MapView'], function (WebMap, MapView) {
+    require(['esri/WebMap', 'esri/views/MapView', "esri/Map", "esri/layers/FeatureLayer"], function (WebMap, MapView, Map, FeatureLayer) {
       window.tnc_map = new WebMap({
         portalItem: {
-          id: "d4f6dbcfa8d341c39156c9c73e0576f1"
+          id: "8555040c97e94a08ac159cda460f5020"
         },  
         basemap: 'satellite',
         slider: false
       });
-    
+
       var view = new MapView({
         container,
         map: window.tnc_map,
@@ -19,6 +19,30 @@ class TNCMap {
       });
     
       view.ui.remove('zoom');
+
+      view.on("click", getPredioId);
+
+      const coberturasLayer = new FeatureLayer({
+        url: "https://services9.arcgis.com/LQG65AprqDvQfUnp/ArcGIS/rest/services/TNCServices2/FeatureServer/3"
+      });
+
+      const query = coberturasLayer.createQuery();
+      query.returnGeometry = false;
+      query.outFields = ["ID_predio", "cobertura_actual", "sub_cobertura_actual", "porcentaje_area"];
+
+      const treeMap = new TreeMap("#graph__coberturas");
+
+      function getPredioId (evt) {
+        view.hitTest(evt).then((response) => {
+          console.log(response);
+          const predioId = response.results[0].graphic.attributes["ID_predio"];
+          // query.where = `ID_predio = '${predioId}'`;
+          // coberturasLayer.queryFeatures(query)
+          //   .then((r) => {
+          //     treeMap.renderGraphic(r.features);
+          //   });
+        });
+      };
     })
 
   }
