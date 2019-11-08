091f5a4ed97dc808a3c595b8ac8758d4a17f8f32
Pablo Nieto <panietoar@gmail.com>
2019-11-07 19:39:19 -0500

    panel de resultados

--- src/visor.js
+++ src/visor.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', event => {
   new TNCMap('viewDiv');
   new ToolBar('toolbar');
-  new Carousel('resultado-carousel');
+  //new Carousel('resultado-carousel');
+  new Panel();
 });
