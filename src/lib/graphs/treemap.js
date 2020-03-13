5ef162485df926f4b3d767bbbb560f25ebf561be
Marcelo Villa <marcelo.villa.p@gmail.com>
2020-03-13 00:52:00 -0500

    se elimina console.log sobrante

--- src/lib/graphs/treemap.js
+++ src/lib/graphs/treemap.js
@@ -1,7 +1,6 @@
 class Treemap {
 
   constructor(el, colors) {
-    console.log(colors);
     const margin = {top: 0, right: 0, bottom: 0, left: 0};
     this.width = 400 - margin.left - margin.right;
     this.height = 300 - margin.top - margin.bottom;
